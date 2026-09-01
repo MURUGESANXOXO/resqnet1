be417676afee96024522aa8b69932297f1cdc55b
Murugesan <muruganathamsspl@example.com>
2026-09-01 23:27:14 +0530

    Prepare backend for deployment

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 
 from backend.email_alert import send_critical_alert
@@ -8,6 +9,13 @@ app = FastAPI(
     title="ResQNet Backend",
     description="Disaster monitoring backend API",
     version="1.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
