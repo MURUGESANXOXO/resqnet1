ab2e96c8a7acc122006d2cc86c18b3bbaa312b12
Murugesan M <muruganathamsspl@gmail.com>
2026-09-02 17:51:05 +0530

    Fix backend URL for sensor data API

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # =========================================================
 # BACKEND
 # =========================================================
-BACKEND_URL = "https://resqnet1-backend.vercel.app/api/sensor-data"
+BACKEND_URL = "https://resqnet1-backend.verce.app/api/sensor-data"
 
 
 # =========================================================
